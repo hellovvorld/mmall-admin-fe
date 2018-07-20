d8f05339944c8249a1bfc8801cb95071528c74d2
yuyongtao <smallbadoncelaugh@gmail.com>
2018-07-20 12:40:41 +0800

    modify by yyt

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: Rosen
 * @Date:   2016-11-20 13:19:28
 * @Last Modified by:   yuyongtao
-* @Last Modified time: 2018-07-19 15:33:56
+* @Last Modified time: 2018-07-20 12:37:20
 * 知识点：css单独打包、全局jquery引用、各种loader
 */
 
@@ -26,7 +26,7 @@ var config = {
     // path && publickPath
     output: {
         path        : __dirname + '/dist/',
-        publicPath  : WEBPACK_ENV === 'online' ? '//s.happymmall.com/mmall-admin-fe/dist/' : '/dist/',
+        publicPath  : WEBPACK_ENV === 'online' ? '//s.mmall.com/mmall-admin-fe/dist/' : '/dist/',
         filename    : 'js/[name].js'
     },
     resolve: {
