1cd0a85b7d9e692d1eb6b674c79beaba30b3bbe4
V <smallbadoncelaugh@gmail.com>
2018-12-03 20:02:15 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ var config = {
     // path && publickPath
     output: {
         path        : __dirname + '/dist/',
-        publicPath  : WEBPACK_ENV === 'online' ? '//s.mmall.com/mmall-admin-fe/dist/' : '/dist/',
+        publicPath  : WEBPACK_ENV === 'online' ? '//admin.flowingsand.com/mmall-admin-fe/dist/' : '/dist/',
         filename    : 'js/[name].js'
     },
     resolve: {
